e63a40f3c45f4863621c2a2b6265cf18afcab355
hardi <hardik.munjaal@gmail.com>
2016-05-13 18:23:12 +0530

    saved imortant urls

---
+++ impUrl.js
@@ -0,0 +1,7 @@
+http://bl.ocks.org/biovisualize/5372077
+http://www.w3schools.com/svg/svg_grad_radial.asp
+http://krispo.github.io/angular-nvd3/#/
+http://plnkr.co/edit/6t5bky?p=preview
+http://help.qlik.com/en-US/sense-developer/2.2/Subsystems/Mashups/Content/Howtos/mashups-use-angularjs-in-mashup.htm
+http://help.qlik.com/en-US/sense-developer/2.2/Subsystems/Dev-Hub/Content/Overview/dev-hub-at-a-glance.htm
+https://community.qlik.com/thread/212853
